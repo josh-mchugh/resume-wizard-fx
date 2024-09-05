8d215d417e1c1d6817f555d623b214ebbc2a3119
Josh McHugh <joshua.mchugh01@gmail.com>
2024-09-04 21:43:39 -0400

    ScalaFX Hello World

---
+++ build.sc
@@ -0,0 +1,21 @@
+import mill._, scalalib._
+
+object app extends RootModule with ScalaModule {
+  def scalaVersion = "3.3.3"
+
+  def resolutionCustomizer = T.task {
+     Some( (r: coursier.core.Resolution) =>
+       r.withOsInfo(coursier.core.Activation.Os.fromProperties(sys.props.toMap))
+     )
+   }
+
+  def ivyDeps = {
+    Agg(
+      ivy"org.scalafx::scalafx:22.0.0-R33",
+      ivy"org.controlsfx:controlsfx:11.2.1",
+      ivy"org.openjfx:javafx-base:22.0.2",
+      ivy"org.openjfx:javafx-controls:22.0.2",
+      ivy"org.openjfx:javafx-web:22.0.2",
+    )
+  }
+}
---
+++ src/net/sailware/resumewizard/Application.scala
@@ -0,0 +1,22 @@
+package net.sailware.resumewizard
+
+import scalafx.Includes.*
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.geometry.VPos
+import scalafx.scene.Scene
+import scalafx.scene.text.{Font, FontWeight, Text}
+
+object Application extends JFXApp3:
+
+  override def start(): Unit =
+    val text = new Text("Resume Wizard"):
+      textOrigin = VPos.Top
+      font = Font.font(null, FontWeight.Bold, 18)
+
+    stage = new PrimaryStage:
+      scene = new Scene(400, 100):
+        content = text
+
+    text.layoutX <== (stage.scene().width - text.prefWidth(-1)) / 2
+    text.layoutY <== (stage.scene().height - text.prefHeight(-1)) / 2
