a46fb8d9fa86f69dbb2ddf988349a1f16bb0fb62
Josh McHugh <joshua.mchugh01@gmail.com>
2024-09-06 00:43:23 -0400

    Trying to follow along with some Atlantafx code

--- src/net/sailware/resumewizard/Application.scala
+++ src/net/sailware/resumewizard/Application.scala
@@ -3,20 +3,46 @@ package net.sailware.resumewizard
 import scalafx.Includes.*
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.geometry.Pos
 import scalafx.geometry.VPos
+import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.layout.AnchorPane
+import scalafx.scene.layout.BorderPane
+import scalafx.scene.layout.StackPane
+import scalafx.scene.layout.HBox
+import scalafx.scene.layout.VBox
 import scalafx.scene.text.{Font, FontWeight, Text}
+import scalafx.stage.Stage
 
 object Application extends JFXApp3:
 
   override def start(): Unit =
-    val text = new Text("Resume Wizard"):
-      textOrigin = VPos.Top
-      font = Font.font(null, FontWeight.Bold, 18)
-
     stage = new PrimaryStage:
-      scene = new Scene(400, 100):
-        content = text
+      scene = new Scene(1280, 768):
+        root = new ApplicationWindow
 
-    text.layoutX <== (stage.scene().width - text.prefWidth(-1)) / 2
-    text.layoutY <== (stage.scene().height - text.prefHeight(-1)) / 2
+class ApplicationWindow extends AnchorPane:
+  val body = new StackPane:
+    children = new MainLayer
+
+  children = body
+
+class MainLayer extends BorderPane:
+
+  val topText = new Text("Resume Wizard Top"):
+    textOrigin = VPos.Top
+    font = Font.font(null, FontWeight.Bold, 18)
+
+  val centerText = new Text("Resume Wizard Center"):
+    textOrigin = VPos.Top
+    font = Font.font(null, FontWeight.Bold, 18)
+
+  top = topText
+  center = new HBox:
+      style = "--fx-border-color: red; -fx-border-width: 1; -fx-border-style: solid;"
+      alignment = Pos.CENTER
+      children = new VBox
+        alignment = Pos.CENTER
+        children = centerText
+      
