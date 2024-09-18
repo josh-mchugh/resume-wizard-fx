439984472cf5f74ae4e490c869f36f87dac60e79
Josh McHugh <joshua.mchugh01@gmail.com>
2024-09-18 00:19:12 -0400

    Refactored new resume layout into MVCI pattern

--- src/net/sailware/resumewizard/Application.scala
+++ src/net/sailware/resumewizard/Application.scala
@@ -5,6 +5,7 @@ import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.event.ActionEvent
 import scalafx.geometry.VPos
+import scalafx.scene.Node
 import scalafx.scene.Scene
 import scalafx.scene.control.Button
 import scalafx.scene.control.Label
@@ -13,10 +14,10 @@ import scalafx.scene.control.ScrollPane
 import scalafx.scene.control.ScrollPane.ScrollBarPolicy
 import scalafx.scene.layout.AnchorPane
 import scalafx.scene.layout.BorderPane
-import scalafx.scene.layout.VBox
 import scalafx.scene.layout.Priority
 import scalafx.scene.layout.Region
 import scalafx.scene.layout.StackPane
+import scalafx.scene.layout.VBox
 import scalafx.scene.text.{Font, FontWeight, Text, TextFlow}
 import scalafx.stage.Stage
 import javafx.util.Builder
@@ -27,27 +28,6 @@ object Main extends JFXApp3:
     stage = new PrimaryStage:
       scene = new Scene(1280, 768):
         root = new MainController().view()
-
-  def createResumePane =
-    new VBox(5):
-      style = """
-              -fx-padding: 20;
-              -fx-background-color: #ffffff;
-              -fx-background-radius: 5;
-              -fx-border-color: rgba(0, 0, 0, 0.18); 
-              -fx-border-width: 1; 
-              -fx-border-style: solid;
-              -fx-border-radius: 5;
-              -fx-effect: dropshadow(three-pass-box, rgba(0, 0, 0, 0.18), 10, 0, 0, 0);
-              """
-      maxWidth = 1020
-      children = List(
-        new Text("Create New Resume"),
-        new Button("Create Resume"),
-        new Label("Resume Name") { },
-        new TextField { }
-      )
-
 
 /*
   Main Components
@@ -95,7 +75,13 @@ class MainViewBuilder(val model: MainModel) extends Builder[Region]:
       fitToHeight = true
       hbarPolicy = ScrollBarPolicy.NEVER
       fitToWidth = true
-        content = new DashboardController().view()
+      content = createDashboardPane
+
+  def createDashboardPane =
+    new StackPane:
+      style = "-fx-padding: 20;"
+      //children = new DashboardController().view()
+      children = new NewResumeController().view()
 
 /*
   Dashboard Components
@@ -106,19 +92,52 @@ class DashboardController:
   val model = new DashboardModel
   val viewBuilder = new DashboardViewBuilder(model) 
 
+  def view(): Node =
+    viewBuilder.build()
+
+class DashboardViewBuilder(val model: DashboardModel) extends Builder[Node]:
+
+  override def build(): Node =
+    createResumeButton
+
+  def createResumeButton =
+    new Button("Create Resume")
+//      onAction = (event: ActionEvent) => stackPane.getChildren().add(createResumePane)
+
+/*
+  New Resume Components
+*/
+class NewResumeModel { }
+
+class NewResumeController:
+
+  val model = new NewResumeModel()
+  val viewBuilder = new NewResumeViewBuilder(model)
+
   def view(): Region =
     viewBuilder.build()
 
-class DashboardViewBuilder(val model: DashboardModel) extends Builder[Region]:
+class NewResumeViewBuilder(val model: NewResumeModel) extends Builder[Region]:
 
   override def build(): Region =
-    createDashboardPane
+    createResumePane
 
-  def createDashboardPane =
-    new StackPane:
-      style = "-fx-padding: 20;"
-      children = createResumeButton(this)
-
-  def createResumeButton(stackPane: StackPane) =
-    new Button("Create Resume")
-//      onAction = (event: ActionEvent) => stackPane.getChildren().add(createResumePane)
+  def createResumePane =
+    new VBox(5):
+      style = """
+              -fx-padding: 20;
+              -fx-background-color: #ffffff;
+              -fx-background-radius: 5;
+              -fx-border-color: rgba(0, 0, 0, 0.18); 
+              -fx-border-width: 1; 
+              -fx-border-style: solid;
+              -fx-border-radius: 5;
+              -fx-effect: dropshadow(three-pass-box, rgba(0, 0, 0, 0.18), 10, 0, 0, 0);
+              """
+      maxWidth = 1020
+      children = List(
+        new Text("Create New Resume"),
+        new Button("Create Resume"),
+        new Label("Resume Name") { },
+        new TextField { }
+      )
