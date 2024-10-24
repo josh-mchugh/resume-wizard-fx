0a664db60d46fe0047acd9c7e6a4eed70ca8568b
Josh McHugh <josh.mchugh@mailbox.org>
2024-10-23 22:21:21 -0400

    Started adding Scaladocs for code

--- src/net/sailware/resumewizard/view/dashboard/DashboardController.scala
+++ src/net/sailware/resumewizard/view/dashboard/DashboardController.scala
@@ -4,9 +4,22 @@ import net.sailware.resumewizard.Controller
 
 import scalafx.scene.Node
 
+/**
+  * DashboardController is the controller for the Dashboard page component.
+  * The controller handles the creation of the presenter and view.
+  *
+  * Dashboard component allows for the user to manage their resumes.
+  *
+  * author Josh McHugh
+  */
 class DashboardController extends Controller[Node]:
   val dashboardPresenter = new DashboardPresenterImpl()
   val dashboardView = new DashboardViewImpl(dashboardPresenter)
 
+  /**
+    * Returns the Dashboard component view
+    *
+    * return dashboard view node
+    */
   override def view(): Node =
     dashboardView.view()
--- src/net/sailware/resumewizard/view/dashboard/DashboardPresenter.scala
+++ src/net/sailware/resumewizard/view/dashboard/DashboardPresenter.scala
@@ -1,4 +1,13 @@
 package net.sailware.resumewizard.view.dashboard
 
+/**
+  * Dashboard presenter trait for user interaction with the Dashboard view.
+  *
+  * author Josh McHugh
+  */
 trait DashboardPresenter:
+
+  /**
+    * Handles the user action when triggering the New Resume Button.
+    */
   def onNewResumeButtonAction(): Unit
