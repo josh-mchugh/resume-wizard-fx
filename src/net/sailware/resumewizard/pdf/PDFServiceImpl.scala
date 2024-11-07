745469363cc7302423f0cfae2b4199cf682d2741
Josh McHugh <josh.mchugh@mailbox.org>
2024-11-06 22:10:19 -0500

    Add other parts of the resume in pdf template

--- src/net/sailware/resumewizard/pdf/PDFServiceImpl.scala
+++ src/net/sailware/resumewizard/pdf/PDFServiceImpl.scala
@@ -33,12 +33,51 @@ class PDFServiceImpl() extends PDFService:
     html(
       head(),
       body(
+        // render personal details
         if (resume.hasPersonalDetails())
           div(
             h1(resume.personalDetails.name),
             p(resume.personalDetails.title),
             p(resume.personalDetails.summary)
           )
-        else div()
+        else div(),
+        // render contact details
+        if (resume.hasContactDetails())
+          div(
+            p(resume.contactDetails.phone),
+            p(resume.contactDetails.email),
+            p(resume.contactDetails.location)
+          )
+        else div(),
+        // render socials
+        if (resume.hasSocials())
+          for (social <- resume.socials) yield
+            div(
+              p(social.name),
+              p(social.url)
+            )
+        else div(),
+        // render experiences
+        if (resume.hasExperiences())
+          for (experience <- resume.experiences) yield
+            div(
+              p(experience.title),
+              p(experience.organization),
+              p(experience.duration),
+              p(experience.location),
+              p(experience.description),
+              p(experience.skills)
+            )
+        else div(),
+        // render certifications
+        if(resume.hasCertifications())
+          for (certification <- resume.certifications) yield
+            div(
+              p(certification.title),
+              p(certification.organization),
+              p(certification.duration),
+              p(certification.location)
+            )
+        else div(),
       ),
     ).render
