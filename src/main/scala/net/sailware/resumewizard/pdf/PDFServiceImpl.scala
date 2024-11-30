54bc83bb22edddff7040f3a232d69e5da2513547
Josh McHugh <josh.mchugh@mailbox.org>
2024-11-30 17:45:36 -0500

    Removed FlyingSuacer and added PDFBox for PDF generation

--- src/main/scala/net/sailware/resumewizard/pdf/PDFServiceImpl.scala
+++ src/main/scala/net/sailware/resumewizard/pdf/PDFServiceImpl.scala
@@ -1,84 +1,38 @@
 package net.sailware.resumewizard.pdf
 
 import java.io.File
-import java.io.FileOutputStream
-import java.io.StringReader
 import net.sailware.resumewizard.pdf.model.GeneratePDFRequest
 import net.sailware.resumewizard.pdf.model.GeneratePDFResponse
 import net.sailware.resumewizard.resume.Resume
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.PDPageContentStream
+import org.apache.pdfbox.pdmodel.font.PDType1Font
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts.FontName
 import org.slf4j.LoggerFactory
-import org.w3c.dom.Document
-import org.xhtmlrenderer.pdf.ITextRenderer
-import org.xhtmlrenderer.resource.XMLResource
-import scalatags.Text.all.*
 
 class PDFServiceImpl() extends PDFService:
 
   val logger = LoggerFactory.getLogger(classOf[PDFServiceImpl])
-  val renderer = new ITextRenderer()
 
   override def generatePDF(request: GeneratePDFRequest): GeneratePDFResponse =
 
     val file = new File("resume.pdf")
     try {
-      val fileOutputStream = new FileOutputStream(file)
-      val source = XMLResource.load(new StringReader(generateHTML(request.resume))).getDocument()
-      renderer.createPDF(source, fileOutputStream)
+      val document = new PDDocument()
+      val page = new PDPage()
+      val contentStream = new PDPageContentStream(document, page)
+      contentStream.beginText()
+      contentStream.setFont(new PDType1Font(FontName.TIMES_BOLD_ITALIC), 14)
+      contentStream.newLineAtOffset(0, 0)
+      contentStream.showText(request.resume.personalDetails.name)
+      contentStream.endText()
+      contentStream.close()
+      document.addPage(page)
+      document.save(file)
+      document.close()
     } catch {
       case t: Throwable => logger.error("error generating PDF", t)
     }
 
     GeneratePDFResponse(file)
-
-  private def generateHTML(resume: Resume): String =
-    html(
-      head(),
-      body(
-        // render personal details
-        if (resume.hasPersonalDetails())
-          div(
-            h1(resume.personalDetails.name),
-            p(resume.personalDetails.title),
-            p(resume.personalDetails.summary)
-          )
-        else div(),
-        // render contact details
-        if (resume.hasContactDetails())
-          div(
-            p(resume.contactDetails.phone),
-            p(resume.contactDetails.email),
-            p(resume.contactDetails.location)
-          )
-        else div(),
-        // render socials
-        if (resume.hasSocials())
-          for (social <- resume.socials)
-            yield div(
-              p(social.name),
-              p(social.url)
-            )
-        else div(),
-        // render experiences
-        if (resume.hasExperiences())
-          for (experience <- resume.experiences)
-            yield div(
-              p(experience.title),
-              p(experience.organization),
-              p(experience.duration),
-              p(experience.location),
-              p(experience.description),
-              p(experience.skills)
-            )
-        else div(),
-        // render certifications
-        if (resume.hasCertifications())
-          for (certification <- resume.certifications)
-            yield div(
-              p(certification.title),
-              p(certification.organization),
-              p(certification.duration),
-              p(certification.location)
-            )
-        else div()
-      )
-    ).render
