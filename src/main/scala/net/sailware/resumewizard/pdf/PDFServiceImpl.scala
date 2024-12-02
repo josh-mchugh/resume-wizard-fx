e2a5ff228344e5aef0c2b44224ee6e9a93a471c9
Josh McHugh <josh.mchugh@mailbox.org>
2024-12-01 22:33:03 -0500

    Experimenting with EasyTable for generating PDFs

--- src/main/scala/net/sailware/resumewizard/pdf/PDFServiceImpl.scala
+++ src/main/scala/net/sailware/resumewizard/pdf/PDFServiceImpl.scala
@@ -6,10 +6,19 @@ import net.sailware.resumewizard.pdf.model.GeneratePDFResponse
 import net.sailware.resumewizard.resume.Resume
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.common.PDRectangle
 import org.apache.pdfbox.pdmodel.PDPageContentStream
 import org.apache.pdfbox.pdmodel.font.PDType1Font
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts.FontName
 import org.slf4j.LoggerFactory
+import org.vandeseer.easytable.TableDrawer
+import org.vandeseer.easytable.settings.BorderStyle
+import org.vandeseer.easytable.settings.HorizontalAlignment
+import org.vandeseer.easytable.structure.Row
+import org.vandeseer.easytable.structure.Table
+import org.vandeseer.easytable.structure.cell.TextCell
+
+import java.awt.*
 
 class PDFServiceImpl() extends PDFService:
 
@@ -20,14 +29,40 @@ class PDFServiceImpl() extends PDFService:
     val file = new File("resume.pdf")
     try {
       val document = new PDDocument()
-      val page = new PDPage()
+      val page = new PDPage(PDRectangle.A4)
       val contentStream = new PDPageContentStream(document, page)
-      contentStream.beginText()
-      contentStream.setFont(new PDType1Font(FontName.TIMES_BOLD_ITALIC), 14)
-      contentStream.newLineAtOffset(0, 0)
-      contentStream.showText(request.resume.personalDetails.name)
-      contentStream.endText()
+
+      val myTable = Table.builder()
+        .addColumnsOfWidth(200, 200)
+        .padding(2)
+        .addRow(Row.builder()
+          .add(TextCell.builder().text("One One").borderWidth(4).borderColorLeft(Color.MAGENTA).backgroundColor(Color.WHITE).build())
+          .add(TextCell.builder().text("One Two").borderWidth(0).backgroundColor(Color.YELLOW).build())
+          .build())
+        .addRow(Row.builder()
+          .padding(10)
+          .add(TextCell.builder().text("Two One").textColor(Color.RED).build())
+          .add(TextCell.builder().text("Two Two")
+            .borderWidthRight(1f)
+            .borderStyleRight(BorderStyle.DOTTED)
+            .horizontalAlignment(HorizontalAlignment.RIGHT)
+            .build())
+          .build())
+        .build()
+
+      // Set up the drawer
+      val tableDrawer = TableDrawer.builder()
+        .contentStream(contentStream)
+        .startX(20f)
+        .startY(page.getMediaBox().getUpperRightY() - 20f)
+        .table(myTable)
+        .build()
+
+      // And go for it!
+      tableDrawer.draw()
+
       contentStream.close()
+
       document.addPage(page)
       document.save(file)
       document.close()
