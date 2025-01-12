ce54eebebe36a019b1c4e95f055e7322e5dcf5ac
Josh McHugh <josh.mchugh@mailbox.org>
2025-01-12 17:14:50 -0500

    Fiddling with Pane and generating demo resume

--- src/main/scala/net/sailware/resumewizard/view/resume/preview/PreviewViewImpl.scala
+++ src/main/scala/net/sailware/resumewizard/view/resume/preview/PreviewViewImpl.scala
@@ -2,9 +2,18 @@ package net.sailware.resumewizard.view.resume.preview
 
 import scalafx.Includes.*
 import scalafx.scene.Node
+import scalafx.scene.layout.Pane
 import scalafx.scene.text.Text
 
 class PreviewViewImpl(val model: PreviewModel) extends PreviewView:
 
   override def view(): Node =
-    Text("Hello")
+    new Pane:
+      style = "-fx-background-color: white"
+      maxWidth = 793.7F
+      maxHeight = 1122.52F
+      minWidth = 793.7F
+      minHeight = 1122.52F
+      children = new Text("Hello World"):
+        x = 50
+        y = 50
